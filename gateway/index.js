d10eb97564dbe2e518c8313f8a21a0fdd16c3f94
SanyiBet <sanyicajamarca@gmail.com>
2025-10-12 21:25:42 -0500

    cambio

--- gateway/index.js
+++ gateway/index.js
@@ -12,5 +12,3 @@ console.log('MS_BODEGAS =', process.env.MS_BODEGAS);
 console.log('MS_PRODUCTOS =', process.env.MS_PRODUCTOS);
 console.log('MS_CLIMA =', process.env.MS_CLIMA);
 console.log('PORT =', process.env.PORT);
-
-//Hola
